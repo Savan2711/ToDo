24690c7f212262224efca02abae3ce70c306d01a
Savan2711 <savananadani@gmail.com>
2019-03-20 21:51:15 +0530

    structure of classes are defined and database is implimented

---
+++ ToDo/ToDo/TaskService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TaskService : ITaskService
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        public TaskService()
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user1\Desktop\ToDo\ToDo\ToDoDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+        }
+
+        public bool AddTask(User user, Task task)
+        {
+            if(!user.ValidateUser())
+            {
+                return false;
+            }
+
+            cmd.CommandText = "INSERT INTO Task VALUES(@title, @description, @isCompleted)";
+            cmd.Parameters.AddWithValue("@title", task.title);
+            cmd.Parameters.AddWithValue("@description", task.description);
+            cmd.Parameters.AddWithValue("@isCompleted", task.isCompleted ? 1 : 0);
+
+            con.Open();
+
+            int addedRows = cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            return addedRows == 1 ? true : false;
+        }
+
+        public bool DeleteTask(User user, string taskTitle)
+        {
+            if (!user.ValidateUser())
+            {
+                return false;
+            }
+
+            cmd.CommandText = "DELETE FROM Task WHERE title=@title";
+            cmd.Parameters.AddWithValue("@title", taskTitle);
+            
+            con.Open();
+
+            int addedRows = cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            return addedRows == 1 ? true : false;
+        }
+
+        public IEnumerable<Task> GetAllTasks(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MarkTaskCompleted(User user, string taskTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTask(User user, string taskTitle, Task task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDo/ToDo/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ToDo
+{
+    public class User
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+
+        public bool ValidateUser()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user1\Desktop\ToDo\ToDo\ToDoDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * from User WHERE userName=@userName and password=@password";
+            cmd.Parameters.AddWithValue("@userName", userName);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            con.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            bool isValidated = reader.HasRows;
+            reader.Close();
+
+            con.Close();
+
+            return isValidated;
+        }
+    }
+
+
+}
