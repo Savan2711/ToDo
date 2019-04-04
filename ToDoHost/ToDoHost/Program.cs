a1b069052584b015a477eb9e4306b1f0c0e8bbd9
Savan2711 <savananadani@gmail.com>
2019-04-04 19:51:15 +0530

    Host application is implemented

---
+++ ToDoHost/ToDoHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo;
+
+namespace ToDoHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type userService = typeof(UserService);
+            Type taskService = typeof(TaskService);
+
+            Uri taskTcp = new Uri("net.tcp://localhost:8010/TaskService");
+
+            Uri userTcp = new Uri("net.tcp://localhost:8010/UserService");
+            using (ServiceHost userHost = new ServiceHost(userService, userTcp))
+            using (ServiceHost taskHost = new ServiceHost(userService, taskTcp))
+            {
+                userHost.Open();
+                Console.WriteLine("UserService Published");
+
+                taskHost.Open();
+                Console.WriteLine("TaskService Published");
+                Console.ReadLine();
+                userHost.Close();
+                taskHost.Close();
+            }
+        }
+    }
+}
